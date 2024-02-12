eb32b8297a2973b9fc04148cc391e25a703a07ea
Miras <miamib531@gmail.com>
2024-02-13 00:44:56 +0600

    refactor: refactor code

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,15 +5,33 @@ import { v4 as uuid, validate } from 'uuid'
 
 export class UserService {
 	private database: UserSchema[] = []
-
-	read(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
-		const { url } = req
-		if (url === '/api/users') {
-			return sendSuccess(res, this.database)
+	private getIdFromUrl(url: string | undefined) {
+		if (!url || url === '/api/users') {
+			return null
 		}
-		const id = url?.split('/').at(-1)
+		return url.split('/').at(-1)
+	}
+	private async parseRequestBody(req: IncomingMessage): Promise<UserBody> {
+		return new Promise((resolve, reject) => {
+			const dataChunks: Buffer[] = []
+			req
+				.on('data', (chunk): void => {
+					dataChunks.push(chunk)
+				})
+				.on('end', () => {
+					try {
+						const body = JSON.parse(Buffer.concat(dataChunks).toString())
+						resolve(body)
+					} catch (error) {
+						reject(error)
+					}
+				})
+		})
+	}
+	read(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+		const id = this.getIdFromUrl(req.url)
 		if (!id) {
-			return sendFailure(res, ERROR_MESSAGE.NOT_FOUND)
+			return sendSuccess(res, this.database)
 		}
 		const isValidId = validate(id)
 		if (!isValidId) {
@@ -25,34 +43,26 @@ export class UserService {
 		}
 		return sendSuccess(res, record)
 	}
-	create(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+	create = async (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
 		const { url } = req
 		if (url !== '/api/users') {
 			return sendFailure(res, ERROR_MESSAGE.NOT_FOUND)
 		}
-		const dataChunks: Buffer[] = []
-		req
-			.on('data', (chunk): void => {
-				dataChunks.push(chunk)
-			})
-			.on('end', () => {
-				try {
-					const body = JSON.parse(Buffer.concat(dataChunks).toString())
-					const validationResult = this.validateBody(body)
-					if (validationResult.isValid) {
-						const newUser: UserSchema = { ...body, id: uuid() }
-						this.database.push(newUser)
-						return sendSuccess(res, newUser, STATUS_CODE.CREATED)
-					}
-					return sendFailure(res, [ERROR_MESSAGE.NOT_BODY, ...validationResult.errors].join(', '), STATUS_CODE.BAD_REQUEST)
-				} catch (error) {
-					sendFailure(res, ERROR_MESSAGE.INTERNAL_ERROR, STATUS_CODE.INTERNAL_ERROR)
-				}
-			})
+		try {
+			const body = await this.parseRequestBody(req)
+			const validationResult = this.validateBody(body)
+			if (validationResult.isValid) {
+				const newUser: UserSchema = { ...body, id: uuid() }
+				this.database.push(newUser)
+				return sendSuccess(res, newUser, STATUS_CODE.CREATED)
+			}
+			return sendFailure(res, [ERROR_MESSAGE.NOT_BODY, ...validationResult.errors].join(', '), STATUS_CODE.BAD_REQUEST)
+		} catch (error) {
+			sendFailure(res, ERROR_MESSAGE.INTERNAL_ERROR, STATUS_CODE.INTERNAL_ERROR)
+		}
 	}
 	validateBody(body: UserBody) {
 		const errors: string[] = []
-
 		if (!('username' in body) || typeof body.username !== 'string') {
 			errors.push('Invalid or missing username')
 		}
@@ -62,18 +72,13 @@ export class UserService {
 		if (!('hobbies' in body) || !Array.isArray(body.hobbies) || !body.hobbies.every(hobby => typeof hobby === 'string')) {
 			errors.push('Invalid or missing hobbies')
 		}
-
 		return {
 			isValid: errors.length === 0,
 			errors,
 		}
 	}
-	update(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
-		const { url } = req
-		if (url === '/api/users') {
-			return sendFailure(res, ERROR_MESSAGE.NOT_FOUND)
-		}
-		const id = url?.split('/').at(-1)
+	update = async (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
+		const id = this.getIdFromUrl(req.url)
 		if (!id) {
 			return sendFailure(res, ERROR_MESSAGE.NOT_FOUND)
 		}
@@ -85,32 +90,20 @@ export class UserService {
 		if (recordIndex === -1) {
 			return sendFailure(res, ERROR_MESSAGE.NOT_EXIST, STATUS_CODE.NOT_FOUND)
 		}
-		const dataChunks: Buffer[] = []
-		req
-			.on('data', (chunk): void => {
-				dataChunks.push(chunk)
-			})
-			.on('end', () => {
-				try {
-					const body = JSON.parse(Buffer.concat(dataChunks).toString())
-					const validationResult = this.validateBody(body)
-					if (validationResult.isValid) {
-						this.database[recordIndex] = { ...body, id }
-						return sendSuccess(res, this.database[recordIndex])
-					}
-					return sendFailure(res, [ERROR_MESSAGE.NOT_BODY, ...validationResult.errors].join(', '), STATUS_CODE.BAD_REQUEST)
-				} catch (error) {
-					sendFailure(res, ERROR_MESSAGE.INTERNAL_ERROR, STATUS_CODE.INTERNAL_ERROR)
-				}
-			})
-	}
-
-	delete(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
-		const { url } = req
-		if (url === '/api/users') {
-			return sendFailure(res, ERROR_MESSAGE.NOT_FOUND)
+		try {
+			const body = await this.parseRequestBody(req)
+			const validationResult = this.validateBody(body)
+			if (validationResult.isValid) {
+				this.database[recordIndex] = { ...body, id }
+				return sendSuccess(res, this.database[recordIndex])
+			}
+			return sendFailure(res, [ERROR_MESSAGE.NOT_BODY, ...validationResult.errors].join(', '), STATUS_CODE.BAD_REQUEST)
+		} catch (error) {
+			sendFailure(res, ERROR_MESSAGE.INTERNAL_ERROR, STATUS_CODE.INTERNAL_ERROR)
 		}
-		const id = url?.split('/').at(-1)
+	}
+	delete(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+		const id = this.getIdFromUrl(req.url)
 		if (!id) {
 			return sendFailure(res, ERROR_MESSAGE.NOT_FOUND)
 		}
