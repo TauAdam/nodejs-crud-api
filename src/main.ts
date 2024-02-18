94f41f9d5695b7280aa3633d875f1eee587194a0
Miras <miamib531@gmail.com>
2024-02-18 15:31:28 +0600

    refactor: Update server port configuration

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { sendFailure } from './utils/response'
 import { ERROR_MESSAGE } from './models'
 dotenv.config()
 
-const port = Number(process.env.PORT) || 5000
+const port = parseInt(process.env.PORT || '3000', 10)
 
 const server = createServer((request: IncomingMessage, response: ServerResponse) => {
 	try {
@@ -24,5 +24,5 @@ const server = createServer((request: IncomingMessage, response: ServerResponse)
 })
 
 server.listen(port, () => {
-	console.log(`Server is listening on port: ${port}, and process id: ${process.pid}`)
+	console.log(`Server is running on http://localhost:${port}`)
 })
